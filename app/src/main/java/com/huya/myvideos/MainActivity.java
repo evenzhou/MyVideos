d3146371c8373a9087a8bb8cbee3c02adbbd1496
evenzhou <737576218@qq.com>
2015-12-28 23:11:15 +0800

    实现异步加载

--- app/src/main/java/com/huya/myvideos/MainActivity.java
+++ app/src/main/java/com/huya/myvideos/MainActivity.java
@@ -1,5 +1,6 @@
 package com.huya.myvideos;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,14 +15,26 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.huya.myvideos.provider.GetList;
+import com.huya.myvideos.provider.Video;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 public  class MainActivity extends Activity {
 
@@ -34,33 +47,19 @@ public  class MainActivity extends Activity {
     private ImageLoader imageLoader = ImageLoader.getInstance();
     private DisplayImageOptions options;
     private ListView lv;
+    private ProgressDialog pDialog;
+    private String url = "http://wx.liansuoerp.com/api.php?a=1";
+    private ArrayList<Video> videos;
+    private ItemListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //拉取数据
-        new Thread(new Runnable(){
-            @Override
-            public void run() {
-                try{
-                    list_item = (new GetList()).getData(1);
-                    MainActivity.this.flag = true;
-                } catch (Exception e) {
-                    Log.e("test", e.toString());
-                }
+        pDialog=new ProgressDialog(this);
+        pDialog.setMessage("loading....");
+        pDialog.show();
 
-            }
-        }).start();
-        //等待数据返回
-        while(!flag){
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
         lv = (ListView)findViewById(R.id.list);
         // 使用DisplayImageOptions.Builder()创建DisplayImageOptions
         options = new DisplayImageOptions.Builder()
@@ -71,7 +70,13 @@ public  class MainActivity extends Activity {
                 .cacheOnDisk(true) // 设置下载的图片是否缓存在SD卡中
                // .displayer(new RoundedBitmapDisplayer(20)) // 设置成圆角图片
                 .build(); // 构建完成
-        lv.setAdapter(new ItemListAdapter());
+
+        videos = new ArrayList<Video>();
+        //请求数据
+        fetchItems();
+        adapter = new ItemListAdapter(videos);
+        //将adapter 设置到listviewer
+        lv.setAdapter(adapter);
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -89,7 +94,7 @@ public  class MainActivity extends Activity {
 
         });
     }
- 
+
     @Override
     public void onStart() {
         super.onStart();
@@ -98,22 +103,69 @@ public  class MainActivity extends Activity {
         // See https://g.co/AppIndexing/AndroidStudio for more information.
     }
 
+    private void fetchItems() {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        //塞入到对象中
+                        for (int i=0; i < response.length();i++) {
+                            try {
+                                JSONObject obj = response.getJSONObject(i);
+                                Video video = new Video();
+                                video.setDateline(obj.optString("upload_start_time"));
+                                video.setImage(obj.optString("cover"));
+                                video.setTimes(obj.optString("play_sum"));
+                                video.setVid(obj.optString("vid"));
+                                videos.add(video);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        hidePDialog();
+                        adapter.notifyDataSetChanged();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("main_activity", error.getMessage());
+                        hidePDialog();
+                    }
+                }
+        );
+        UILApplication.getInstance().addToRequestQueue(jsonArrayRequest);
+    }
     @Override
     public void onStop() {
         super.onStop();
     }
 
+    public void onDestroy() {
+        super.onDestroy();
+        hidePDialog();
+    }
+    public void hidePDialog(){
+        if(pDialog!=null)
+        pDialog.dismiss();
+        pDialog=null;
+    }
+
     class ItemListAdapter extends BaseAdapter {
 
+        private ArrayList<Video> videos;
 
+        ItemListAdapter(ArrayList<Video> videos) {
+            this.videos = videos;
+        }
         @Override
         public int getCount() {
-            return list_item.size();
+            return videos.size();
         }
 
         @Override
         public Object getItem(int position) {
-            return list_item.get(position);
+            return videos.get(position);
         }
 
         @Override
@@ -133,25 +185,24 @@ public  class MainActivity extends Activity {
                 convertView = getLayoutInflater().inflate(R.layout.vlist, parent, false);
                 holder = new ViewHolder();
                 holder.title = (TextView) convertView.findViewById(R.id.title);
-                holder.img = (ImageView) convertView.findViewById(R.id.img);
+                holder.image = (ImageView) convertView.findViewById(R.id.img);
                 holder.dateline = (TextView) convertView.findViewById(R.id.dateline);
                 holder.times = (TextView) convertView.findViewById(R.id.times);
                 convertView.setTag(holder);
             } else {
                 holder = (ViewHolder) convertView.getTag();
             }
-            Map<String, String> map = MainActivity.this.list_item.get(position);
-            holder.title.setText(map.get("title"));
-            holder.dateline.setText(map.get("dateline"));
-            holder.times.setText(map.get("times"));
-            String aUrl = "http://static.oschina.net/uploads/img/201208/13122559_L8G0.png";
-            MainActivity.this.imageLoader.displayImage(map.get("image"), holder.img, options);
+            //用videos 的值填充
+            holder.title.setText(videos.get(position).getTitle());
+            holder.dateline.setText(videos.get(position).getDateline());
+            holder.times.setText(videos.get(position).getTimes());
+            MainActivity.this.imageLoader.displayImage(videos.get(position).getImage(), holder.image, options);
             return convertView;
         }
 
 
         class ViewHolder {
-            public ImageView img;
+            public ImageView image;
             public TextView dateline;
             public TextView times;
             public TextView title;
