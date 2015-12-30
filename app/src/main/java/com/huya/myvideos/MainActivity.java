ebec6eb34d0f8bfb304e48bfccc1f5631bedc19e
evenzhou <737576218@qq.com>
2015-12-30 18:24:15 +0800

    继续完善

--- app/src/main/java/com/huya/myvideos/MainActivity.java
+++ app/src/main/java/com/huya/myvideos/MainActivity.java
@@ -1,95 +1,79 @@
 package com.huya.myvideos;
+
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.DialogInterface;
+import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.Volley;
-import com.huya.myvideos.provider.GetList;
+import com.android.volley.toolbox.StringRequest;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.huya.myvideos.provider.Video;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
 
-public  class MainActivity extends Activity implements AbsListView.OnScrollListener {
+public class MainActivity extends Activity implements AbsListView.OnScrollListener {
 
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
-    private List<Map<String, String>> list_item = null;
-    boolean flag = false;
-    private ImageLoader imageLoader = ImageLoader.getInstance();
+
     private DisplayImageOptions options;
     private ListView lv;
     private ProgressDialog pDialog;
-    //private String url = "http://api.v.huya.com/index.php?r=video/list&channelId=lol&appKey=hyapi_cs&pageSize=7";
-    private String url = "http://wx.liansuoerp.com/api.php?";
+    private String url = "http://api.v.huya.com/index.php?r=video/list&channelId=wiiu&appKey=hyapi_cs&pageSize=7";
     private ArrayList<Video> videos;
     private ItemListAdapter adapter;
     private SwipeRefreshLayout swipeRefreshLayout;
-    private TextView bt;
     private ProgressBar pb;
-    // 设置一个最大的数据条数，超过即不再加载
-    private int MaxDateNum = 30;
     private View moreView;
     private int lastVisibleIndex;
-    private Toast toast;
     private int cur_page;
+    private Boolean isEnd;
     private Boolean is_loading;
+    private Gson gson = new Gson();
+    private boolean noMore = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        pDialog=new ProgressDialog(this);
+        pDialog = new ProgressDialog(this);
         pDialog.setMessage("loading....");
         pDialog.show();
         //初始化第一页
         cur_page = 1;
-        lv = (ListView)findViewById(R.id.list);
+        lv = (ListView) findViewById(R.id.list);
         //上拉加载更多
         moreView = getLayoutInflater().inflate(R.layout.load_more, null);
         pb = (ProgressBar) moreView.findViewById(R.id.process);
-        // 使用DisplayImageOptions.Builder()创建DisplayImageOptions
-        options = new DisplayImageOptions.Builder()
-                .showImageOnLoading(R.drawable.l1) // 设置图片下载期间显示的图片
-                .showImageForEmptyUri(R.drawable.l1) // 设置图片Uri为空或是错误的时候显示的图片
-                .showImageOnFail(R.drawable.l1) // 设置图片加载或解码过程中发生错误显示的图片
-                .cacheInMemory(true) // 设置下载的图片是否缓存在内存中
-                .cacheOnDisk(true) // 设置下载的图片是否缓存在SD卡中
-               // .displayer(new RoundedBitmapDisplayer(20)) // 设置成圆角图片
-                .build(); // 构建完成
-
         videos = new ArrayList<Video>();
         //请求数据
         fetchItems(cur_page);
@@ -114,27 +98,19 @@ public  class MainActivity extends Activity implements AbsListView.OnScrollListe
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                videos.clear();
                 cur_page = 1;
-
+                noMore = false;
                 fetchItems(cur_page);
+                Log.e("aaaa", String.valueOf(noMore));
                 swipeRefreshLayout.setRefreshing(false);
             }
         });
-        //swipeRefreshLayout.setColorSchemeResources(android.R.color.background_light, android.R.color.holo_blue_light, android.R.color.holo_green_light, android.R.color.holo_green_light);
+        swipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_light, android.R.color.background_light, android.R.color.holo_green_light, android.R.color.holo_green_light);
 
         lv.setOnScrollListener(this);
         //添加到list 底部
-        //lv.addFooterView(moreView);
-//        bt.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View v) {
-//                fetchItems(cur_page);
-//                bt.setVisibility(View.VISIBLE);
-//                pb.setVisibility(View.GONE);
-//
-//            }
-//        });
+        lv.addFooterView(moreView);
+
     }
 
     @Override
@@ -145,44 +121,53 @@ public  class MainActivity extends Activity implements AbsListView.OnScrollListe
         // See https://g.co/AppIndexing/AndroidStudio for more information.
     }
 
-    private void fetchItems(int page) {
+    private void fetchItems(final int page) {
         url = url + "&page=" + page;
         is_loading = true;
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url,
-                new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        //塞入到对象中
-                        for (int i=0; i < response.length();i++) {
-                            try {
-                                JSONObject obj = response.getJSONObject(i);
-                                Video video = new Video();
-                                video.setDateline(obj.optString("upload_start_time"));
-                                video.setImage(obj.optString("cover"));
-                                video.setTimes(obj.optString("play_sum"));
-                                video.setVid(obj.optString("vid"));
-                                video.setTitle(obj.optString("video_title"));
-                                videos.add(video);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                        hidePDialog();
-                        adapter.notifyDataSetChanged();
-                        is_loading = false;
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("main_activity", error.getMessage());
-                        hidePDialog();
-                        is_loading = false;
-                    }
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //接收参数，用gson
+                Type type = new TypeToken<ArrayList<Video>>() {
+                }.getType();
+                ArrayList<Video> videos1 = gson.fromJson(response, type);
+                if (videos1.isEmpty()) {
+                    noMore = true;
                 }
-        );
-        UILApplication.getInstance().addToRequestQueue(jsonArrayRequest);
+                Log.e("ttttt", String.valueOf(noMore));
+                //如果page 等于 1， 代表下拉刷新，清掉原来的列表
+                if (page == 1) {
+                    if (!videos.isEmpty()) {
+                        videos.clear();
+                    }
+                    videos.addAll(videos1);
+                    //ArrayList<Video>  videos = (ArrayList<Video>) videos1.clone();
+                } else {
+                    //去重
+                    for (Video v:videos1) {
+                        if(videos.contains(v)) {
+                            videos1.remove(v);
+                        }
+                    }
+                    //返回的数据添加到末尾
+                    videos.addAll(videos1);
+                }
+                hidePDialog();
+                adapter.notifyDataSetChanged();
+                pb.setVisibility(View.GONE);
+                is_loading = false;
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("main_activity", error.getMessage());
+                hidePDialog();
+                is_loading = false;
+            }
+        });
+        UILApplication.getInstance().addToRequestQueue(stringRequest);
     }
+
     @Override
     public void onStop() {
         super.onStop();
@@ -192,48 +177,41 @@ public  class MainActivity extends Activity implements AbsListView.OnScrollListe
         super.onDestroy();
         hidePDialog();
     }
-    public void hidePDialog(){
-        if(pDialog!=null)
-        pDialog.dismiss();
-        pDialog=null;
+
+    public void hidePDialog() {
+        if (pDialog != null)
+            pDialog.dismiss();
+        pDialog = null;
     }
 
     @Override
     public void onScrollStateChanged(AbsListView view, int scrollState) {
-//        if (scrollState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE && lastVisibleIndex <= videos.size()) {
-//           fetchItems((int) (Math.random() * 50));
-//            bt.setVisibility(View.VISIBLE);
-//            pb.setVisibility(View.GONE);
-//        }
-//        else {
-//            toast =  Toast.makeText(this, "数据全部加载完成，没有更多数据！", Toast.LENGTH_SHORT);
-//           // toast.show();
-//        }
-
+        if (scrollState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE && noMore && isEnd) {
+            Toast.makeText(this, "数据全部加载完成，没有更多数据！", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
         // 计算最后可见条目的索引
         lastVisibleIndex = firstVisibleItem + visibleItemCount;
-        Log.e("TAG", lastVisibleIndex + "__" + totalItemCount);
-        if (lastVisibleIndex == totalItemCount && !is_loading) {
+        //滑到最后
+        isEnd = lastVisibleIndex == totalItemCount ? true : false;
+        //列表还有更多，且不是在加载中
+        if (isEnd && !is_loading && !noMore) {
             cur_page++;
             pb.setVisibility(View.VISIBLE);
             fetchItems(cur_page);
-            pb.setVisibility(View.GONE);
-            //toast =  Toast.makeText(this, "数据全部加载完成，没有更多数据！", Toast.LENGTH_SHORT);
-            //toast.show();
         }
     }
 
     class ItemListAdapter extends BaseAdapter {
-
+        private ImageLoader imageLoader = ImageLoader.getInstance();
         private ArrayList<Video> videos;
-
         ItemListAdapter(ArrayList<Video> videos) {
             this.videos = videos;
         }
+
         @Override
         public int getCount() {
             return videos.size();
@@ -269,14 +247,12 @@ public  class MainActivity extends Activity implements AbsListView.OnScrollListe
                 holder = (ViewHolder) convertView.getTag();
             }
             //用videos 的值填充
-            holder.title.setText(videos.get(position).getTitle());
-            holder.dateline.setText(videos.get(position).getDateline());
-            holder.times.setText(videos.get(position).getTimes());
-            MainActivity.this.imageLoader.displayImage(videos.get(position).getImage(), holder.image, options);
+            holder.title.setText(videos.get(position).getVideo_title());
+            holder.dateline.setText(videos.get(position).getUpload_start_time());
+            holder.times.setText(videos.get(position).getPlay_sum());
+            imageLoader.displayImage(videos.get(position).getCover(), holder.image);
             return convertView;
         }
-
-
         class ViewHolder {
             public ImageView image;
             public TextView dateline;
--- app/src/main/java/com/huya/myvideos/provider/Video.java
+++ app/src/main/java/com/huya/myvideos/provider/Video.java
@@ -1,52 +1,191 @@
 package com.huya.myvideos.provider;
 
 /**
- * Created by even on 2015-12-29.
+ * Created by even on 2015-12-30.
  */
 public class Video {
-    private String vid;
-    private String title;
-    private String image;
-    private String dateline;
-    private String times;
 
-    public String getVid() {
-        return vid;
-    }
+    /**
+     * vid : 495163
+     * udb : zjw8961
+     * yyuid : 5452110
+     * cover : http://vimg.dwstatic.com/1553/495163/4-220x124.jpg
+     * width : 1104
+     * height : 624
+     * duration : 624.854000
+     * channel : lol
+     * game_id : 1
+     * video_title : UZI和55开SOLO【第四场】
+     * source_name : UZI和55开的爆笑SOLO赛3.mpeg.mp4
+     * source_size : 262406461
+     * update_time : 2015-12-30 10:47:17
+     * upload_end_time : 2015-12-30 10:39:06
+     * upload_start_time : 2015-12-30 10:26:32
+     * zan_sum : 0
+     * play_sum : 1
+     * comment_sum : 0
+     */
+
+    private String vid;
+    private String udb;
+    private String yyuid;
+    private String cover;
+    private String width;
+    private String height;
+    private String duration;
+    private String channel;
+    private String game_id;
+    private String video_title;
+    private String source_name;
+    private String source_size;
+    private String update_time;
+    private String upload_end_time;
+    private String upload_start_time;
+    private String zan_sum;
+    private String play_sum;
+    private String comment_sum;
 
     public void setVid(String vid) {
         this.vid = vid;
     }
 
-    public String getTitle() {
-        return title;
+    public void setUdb(String udb) {
+        this.udb = udb;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setYyuid(String yyuid) {
+        this.yyuid = yyuid;
     }
 
-    public String getImage() {
-        return image;
+    public void setCover(String cover) {
+        this.cover = cover;
     }
 
-    public void setImage(String image) {
-        this.image = image;
+    public void setWidth(String width) {
+        this.width = width;
     }
 
-    public String getDateline() {
-        return dateline;
+    public void setHeight(String height) {
+        this.height = height;
     }
 
-    public void setDateline(String dateline) {
-        this.dateline = dateline;
+    public void setDuration(String duration) {
+        this.duration = duration;
     }
 
-    public String getTimes() {
-        return times;
+    public void setChannel(String channel) {
+        this.channel = channel;
     }
 
-    public void setTimes(String times) {
-        this.times = times;
+    public void setGame_id(String game_id) {
+        this.game_id = game_id;
+    }
+
+    public void setVideo_title(String video_title) {
+        this.video_title = video_title;
+    }
+
+    public void setSource_name(String source_name) {
+        this.source_name = source_name;
+    }
+
+    public void setSource_size(String source_size) {
+        this.source_size = source_size;
+    }
+
+    public void setUpdate_time(String update_time) {
+        this.update_time = update_time;
+    }
+
+    public void setUpload_end_time(String upload_end_time) {
+        this.upload_end_time = upload_end_time;
+    }
+
+    public void setUpload_start_time(String upload_start_time) {
+        this.upload_start_time = upload_start_time;
+    }
+
+    public void setZan_sum(String zan_sum) {
+        this.zan_sum = zan_sum;
+    }
+
+    public void setPlay_sum(String play_sum) {
+        this.play_sum = play_sum;
+    }
+
+    public void setComment_sum(String comment_sum) {
+        this.comment_sum = comment_sum;
+    }
+
+    public String getVid() {
+        return vid;
+    }
+
+    public String getUdb() {
+        return udb;
+    }
+
+    public String getYyuid() {
+        return yyuid;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public String getWidth() {
+        return width;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public String getChannel() {
+        return channel;
+    }
+
+    public String getGame_id() {
+        return game_id;
+    }
+
+    public String getVideo_title() {
+        return video_title;
+    }
+
+    public String getSource_name() {
+        return source_name;
+    }
+
+    public String getSource_size() {
+        return source_size;
+    }
+
+    public String getUpdate_time() {
+        return update_time;
+    }
+
+    public String getUpload_end_time() {
+        return upload_end_time;
+    }
+
+    public String getUpload_start_time() {
+        return upload_start_time;
+    }
+
+    public String getZan_sum() {
+        return zan_sum;
+    }
+
+    public String getPlay_sum() {
+        return play_sum;
+    }
+
+    public String getComment_sum() {
+        return comment_sum;
     }
 }
